80e8c1bb2962b5d4f94030987f6c1f427b65f10a
nikolaytuz <32246428+nikolaytuz@users.noreply.github.com>
2025-08-17 22:40:35 +0400

    Show disconnect message and add README

---
+++ log-battle/public/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  grid: { cols: 10, rows: 16, cell: 32, margin: 16 },
+  mana: { regenPerSec: 1, max: 10, tickMs: 100 },
+  costs: { unit: 5, building: 10 },
+  unit: { speedCellsPerSec: 3, radiusPx: 10 },
+  building: { spawnPeriodMs: 2000 },
+  log: { pushCells: 1, width: 28, height: 120, jitterMs: 120 },
+  net: { tickMs: 100 },
+  countdownSec: 3,
+};
+
